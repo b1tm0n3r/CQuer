21db2275fcbe19952ffcb482482c50aefe92e58e
Ziwerr <bartosz.pilarczyk11@gmail.com>
2021-03-16 20:47:36 +0100

    Change MVC Startup port & add simple client to communicate with WebAPI.

--- CQuerMVC/Controllers/HomeController.cs
+++ CQuerMVC/Controllers/HomeController.cs
@@ -1,7 +1,13 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using Common.DTOs;
 using CQuerMVC.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace CQuerMVC.Controllers
 {
@@ -19,6 +25,32 @@ namespace CQuerMVC.Controllers
             return View();
         }
         public IActionResult Privacy()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public IActionResult Login(LoginDto loginDto)
+        {
+            //TODO: Create correct API client
+            HttpClient client = new HttpClient();
+                
+            var uri = new Uri("https://localhost:6001/api/account/login");
+            var json = JsonConvert.SerializeObject(loginDto);
+                
+            HttpContent stringContent = new StringContent(json, Encoding.UTF8, "application/json"); 
+                
+            var postResult = client.PostAsync(uri, stringContent).Result;
+
+            if (postResult.StatusCode == HttpStatusCode.OK)
+            {
+                return RedirectToAction("UserPanel");
+            }
+
+            return View("Index");
+        }
+
+        public IActionResult UserPanel()
         {
             return View();
         }
