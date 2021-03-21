bfefd5e3ad38119fbdaf094096273863232e3bcd
MkTech-0-8YT3 <mktech@protonmail.com>
2021-03-21 17:26:49 +0100

    TDD Example - prepare class with test

---
+++ Common/AppSettingsValidator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common
+{
+    public static class AppSettingsValidator
+    {
+        public static bool IsFileStorePathValid(string fileStorePath)
+        {
+            return true;
+        }
+        public static bool IsLocalApiUrlValid(string localApiUrl)
+        {
+            return true;
+        }
+
+        public static bool IsConnectionStringValid(string databaseConnectionString)
+        {
+            return true;
+        }
+        private static bool CanEstablishConnectionWithDatabase()
+        {
+            return true;
+        }
+    }
+}
---
+++ CommonTests/AppSettingsValidatorTests.cs
@@ -0,0 +1,10 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CommonTests
+{
+    [TestClass]
+    public class AppSettingsValidatorTests
+    {
+        
+    }
+}
