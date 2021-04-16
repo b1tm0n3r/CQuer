70669b5f95e5b453c6b759c0c8d6e4a2a5a45afd
MkTech-0-8YT3 <mktech@protonmail.com>
2021-04-16 20:28:14 +0200

    Add Recursive Crawling Method

--- CommonServices/HttpWebProxy/WebCrawler.cs
+++ CommonServices/HttpWebProxy/WebCrawler.cs
@@ -1,6 +1,7 @@
 ﻿using HtmlAgilityPack;
 using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Text;
 using System.Text.RegularExpressions;
 
@@ -8,29 +9,55 @@ namespace CommonServices.HttpWebProxy
 {
     public class WebCrawler
     {
+        private static readonly string HTTP_HEAD = "HEAD";
+        private static readonly string HTML_RESPONSE_TYPE = "text/html";
         private readonly string baseUrl;
         private readonly HtmlWeb _htmlWeb;
+        private readonly HtmlParser _htmlParser;
         public WebCrawler(string url)
         {
             _htmlWeb = new HtmlWeb();
+            _htmlParser = new HtmlParser();
             baseUrl = ExtractBaseUrl(url);
-        }
-        public List<HtmlDocument> Crawl(List<string> urls)
-        {
-            List<HtmlDocument> results = new List<HtmlDocument>();
-            urls.ForEach(url =>
-            {
-                if(url.StartsWith(baseUrl))
-                    results.Add(_htmlWeb.Load(url));
-            });
-            return results;
         }
         public HtmlDocument Crawl(string url)
         {
             return url.StartsWith(baseUrl) ? _htmlWeb.Load(url) : null;
         }
+        public List<HtmlDocument> Crawl(string url, int depth)
+        {
+            List<HtmlDocument> results = new List<HtmlDocument>();
+            var urlsToCrawl = _htmlParser.GetUrlsFromHtmlDocumentLinks(Crawl(url));
+            Crawl(urlsToCrawl, depth - 1, results);
+            return results;
+        }
 
-        //TODO: Add recursive crawling for given depth
+        private void Crawl(List<string> urls, int depth, List<HtmlDocument> results)
+        {
+            if (depth == 0)
+                return;
+            urls.ForEach(url =>
+            {
+                WebResponse response = RequestHeadFromUrl(url);
+                if (url.StartsWith(baseUrl) && response.ContentType.Contains(HTML_RESPONSE_TYPE))
+                {
+                    var crawledSite = Crawl(url);
+                    results.Add(crawledSite);
+                    var urlsToCrawl = _htmlParser.GetUrlsFromHtmlDocumentLinks(crawledSite);
+                    Crawl(urlsToCrawl, depth - 1, results);
+                }
+                else
+                    return;
+            });
+        }
+
+        //TODO: Might want to change this one to HttpClient
+        private static WebResponse RequestHeadFromUrl(string url)
+        {
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = HTTP_HEAD;
+            return request.GetResponse();
+        }
 
         public string ExtractBaseUrl(string url)
         {
