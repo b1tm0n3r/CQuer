d3a87809c5d95e1b2289a9c31dd2df20de2469c5
madejowy <madejowy12@gmail.com>
2021-04-17 22:23:51 +0200

    Add TicketController

---
+++ CQuerMVC/Controllers/TicketController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CQuerMVC.Controllers
+{
+    public class TicketController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult Edit()
+        {
+            return View();
+        }
+        public IActionResult Delete()
+        {
+            return View();
+        }
+
+    }
+}
