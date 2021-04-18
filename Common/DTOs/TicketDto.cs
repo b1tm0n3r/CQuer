9067738dfd1943c02c6a21d83bedaa760c6b4d2a
madejowy <madejowy12@gmail.com>
2021-04-18 21:03:03 +0200

    Add TicketDto

--- Common/DTOs/TicketDto.cs
+++ Common/DTOs/TicketDto.cs
@@ -1,15 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Common.DTOs
 {
-    class TicketDto
+    public class TicketDto
     {
+        [Required]
         public int CreatorId { get; set; }
+        [Required]
         public string Description { get; set; }
+        [Required]
+        [Range(1, 5)]
         public int Severity { get; set; }
+        [Required]
         public bool Solved { get; set; }
+        [Required]
         public string DownloadUrl { get; set; }
         public string Sha256Checksum { get; set; }
     }
