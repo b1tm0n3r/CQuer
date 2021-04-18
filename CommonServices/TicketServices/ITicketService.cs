25d882f752362ff729c3476ef868028b061dde97
madejowy <madejowy12@gmail.com>
2021-04-18 21:01:26 +0200

    Add ITicketInterface

---
+++ CommonServices/TicketServices/ITicketService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Common.DTOs;
+
+namespace CommonServices.TicketServices
+{
+    interface ITicketService
+    {
+        Task<IEnumerable<TicketDto>> GetTickets();
+        Task<int> Create(TicketDto ticketDto);
+        Task<bool> Edit(int id, TicketDto ticketDto);
+        Task<bool> Delete(int id, TicketDto ticketDto);
+        Task<bool> Sumbit(int id);
+
+    }
+}
