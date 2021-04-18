18c55aa2d0337c1d7f4b49d42f1f5bfb48bca6fc
madejowy <madejowy12@gmail.com>
2021-04-18 20:08:43 +0200

    Create TicketDto

---
+++ Common/DTOs/TicketDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.DTOs
+{
+    class TicketDto
+    {
+        public int CreatorId { get; set; }
+        public string Description { get; set; }
+        public int Severity { get; set; }
+        public bool Solved { get; set; }
+        public string DownloadUrl { get; set; }
+        public string Sha256Checksum { get; set; }
+    }
+}
