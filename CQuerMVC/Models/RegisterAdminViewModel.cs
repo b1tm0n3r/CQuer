c1c88c7905b69212630409a8f66cc373266af379
Ziwerr <bartosz.pilarczyk11@gmail.com>
2021-05-15 17:39:57 +0200

    Add RegisterAdminViewModel

---
+++ CQuerMVC/Models/RegisterAdminViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Common.DataModels.IdentityManagement;
+using Common.DTOs;
+
+namespace CQuerMVC.Models
+{
+    public class RegisterAdminViewModel : RegisterDto
+    {
+        public AccountType AccountType { get; set; } = AccountType.Administrator;
+    }
+}
