3d01bff3b3b323e9fc2bc477279e78041af93ba5
MkTech-0-8YT3 <mktech@protonmail.com>
2021-06-13 16:56:40 +0200

    Add TicketDtoValidator with unit tests

---
+++ Common/Validators/TicketDtoValidator.cs
@@ -0,0 +1,28 @@
+﻿using Common.DTOs;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Common.Validators
+{
+    public class TicketDtoValidator : AbstractValidator<TicketDto>
+    {
+        public TicketDtoValidator()
+        {
+            RuleFor(x => x.DownloadUrl).NotNull().WithMessage("Download URL cannot be null")
+                .NotEmpty().WithMessage("Download URL cannot be empty");
+
+            RuleFor(x => x.Severity).GreaterThan(1).WithMessage("Severity minimum value is 1")
+                .LessThan(5).WithMessage("Severity maximum value is 5");
+
+            RuleFor(x => x.Description).Must(CheckDescription).WithMessage("Special characters not allowed in description");
+        }
+        private bool CheckDescription(string description)
+        {
+            var regex = new Regex(@"[\^{}$<>;_=\|~\\]");
+            return !regex.IsMatch(description);
+        }
+    }
+}
---
+++ CommonTests/validators/TicketDtoValidatorTests.cs
@@ -0,0 +1,117 @@
+﻿using Common.DTOs;
+using Common.Validators;
+using FluentValidation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonTests.validators
+{
+    [TestClass]
+    public class TicketDtoValidatorTests
+    {
+        [TestMethod]
+        public void RETURN_TRUE_WITH_VALID_DATA()
+        {
+            TicketDtoValidator objectUnderTest = new TicketDtoValidator();
+
+            TicketDto ticketDto = new TicketDto()
+            {
+                Description = "test",
+                DownloadUrl = "http://127.0.0.1/test",
+                Severity = 2,
+                Sha256Checksum = "",
+                Solved = false,
+                Username = "test"
+            };
+
+            var result = objectUnderTest.Validate(ticketDto);
+
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.IsValid);
+        }
+        [TestMethod]
+        public void RETURN_FALSE_WITH_SEVERITY_LESS_THAN_ONE()
+        {
+            TicketDtoValidator objectUnderTest = new TicketDtoValidator();
+
+            TicketDto ticketDto = new TicketDto()
+            {
+                Description = "test",
+                DownloadUrl = "http://127.0.0.1/test",
+                Severity = -1,
+                Sha256Checksum = "",
+                Solved = false,
+                Username = "test"
+            };
+
+            var result = objectUnderTest.Validate(ticketDto);
+
+            Assert.IsNotNull(result);
+            Assert.IsFalse(result.IsValid);
+        }
+        [TestMethod]
+        public void RETURN_FALSE_WITH_SEVERITY_GREATER_THAN_5()
+        {
+            TicketDtoValidator objectUnderTest = new TicketDtoValidator();
+
+            TicketDto ticketDto = new TicketDto()
+            {
+                Description = "test",
+                DownloadUrl = "http://127.0.0.1/test",
+                Severity = 6,
+                Sha256Checksum = "",
+                Solved = false,
+                Username = "test"
+            };
+
+            var result = objectUnderTest.Validate(ticketDto);
+
+            Assert.IsNotNull(result);
+            Assert.IsFalse(result.IsValid);
+        }
+        [TestMethod]
+        public void RETURN_FALSE_IF_DESCRIPTION_CONTAINS_BAD_CHAR()
+        {
+            TicketDtoValidator objectUnderTest = new TicketDtoValidator();
+
+            TicketDto ticketDto = new TicketDto()
+            {
+                Description = "<test>test|$test</test>",
+                DownloadUrl = "http://127.0.0.1/test",
+                Severity = -1,
+                Sha256Checksum = "",
+                Solved = false,
+                Username = "test"
+            };
+
+            var result = objectUnderTest.Validate(ticketDto);
+
+            Assert.IsNotNull(result);
+            Assert.IsFalse(result.IsValid);
+        }
+        [TestMethod]
+        public void RETURN_FALSE_ON_EMPTY_DOWNLOAD_URL()
+        {
+            TicketDtoValidator objectUnderTest = new TicketDtoValidator();
+
+            TicketDto ticketDto = new TicketDto()
+            {
+                Description = "test",
+                DownloadUrl = "",
+                Severity = -1,
+                Sha256Checksum = "",
+                Solved = false,
+                Username = "test"
+            };
+
+            var result = objectUnderTest.Validate(ticketDto);
+
+            Assert.IsNotNull(result);
+            Assert.IsFalse(result.IsValid);
+        }
+    }
+}
